cb78173a398784a1c2b8eb87b98a035606d665c1
NateRiz <riziknat@msu.edu>
2020-01-01 17:24:43 -0500

    readme

---
+++ README.md
@@ -0,0 +1,17 @@
+#LGPy
+LGPy is a linear genetic programming library implemented in Python to evolve problems. In linear programming, a sequence of assembly-like instructions dictates the flow of a program. Each hardware in LGPy contains a set of instructions and a memory buffer that acts as its program.
+
+##Installing
+```pip install LGPy```
+
+##Examples
+Located in: ```LGPy/Examples/```
+
+```python3 even_odd.py```
+
+
+##Authors
+* NateRiz
+
+##Acknowledgements
+* Inspiration from SignalGP: https://github.com/devosoft/Empirical
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_desc = readme.read()
+
+setuptools.setup(
+    name="LGPy",
+    version = "1.0.0",
+    author="NateRiz",
+    author_email="nathan.rizik@gmail.com",
+    description="A Linear Genetic Programming Library",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nateriz/lgpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.6"
+)
